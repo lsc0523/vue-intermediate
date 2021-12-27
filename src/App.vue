b07650388f316efd64e5ba8fbb2d4a2bf2fd653f
이승창 <dltmdckd0523@gmail.com>
2021-12-27 22:11:24 +0900

    refactoring #1

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,9 @@
   <div id="app">
 
     <todo-header></todo-header>
-    <todo-input></todo-input>
-    <todo-list></todo-list>
-    <todo-footer></todo-footer>
+    <todo-input v-on:addItem="addOneItem"></todo-input>
+    <todo-list v-bind:propsdata="todoItems" v-on:deleteItem="deleteOneItem" v-on:toggleItem="toggleOneItem"></todo-list>
+    <todo-footer v-on:clearAll="clearAll"></todo-footer>
 
   </div>
 </template>
@@ -22,6 +22,40 @@ export default {
     'TodoInput': TodoInput,
     'TodoList': TodoList,
     'TodoFooter': TodoFooter
+  },
+  data: function (){
+    return{
+      todoItems : []
+    }
+  },
+  methods: {
+    addOneItem: function (Item){
+      let obj = {completed: false, item: Item};
+      localStorage.setItem(Item,JSON.stringify(obj));
+      this.todoItems.push(obj);
+    },
+    deleteOneItem: function(todoItem,index){
+      localStorage.removeItem(todoItem.item);
+      this.todoItems.splice(index, 1)
+    },
+    toggleOneItem: function (todoItem,index){
+      this.todoItems[index].completed = !this.todoItems[index].completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
+    },
+    clearAll: function(){
+      localStorage.clear();
+      this.todoItems = [];
+    }
+  },
+  created: function(){
+    if (localStorage.length > 0){
+      for (let i = 0; i < localStorage.length; i++) {
+        if(localStorage.key(i) !== 'loglevel:webpack-dev-server'){
+          this.todoItems.push(JSON.parse(localStorage.getItem(localStorage.key(i))));
+        }
+      }
+    }
   }
 }
 </script>
--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -11,7 +11,7 @@ export default {
   name: "TodoFooter",
   methods:{
     clearTodo: function (){
-      localStorage.clear();
+      this.$emit("clearAll");
     }
   }
 }
